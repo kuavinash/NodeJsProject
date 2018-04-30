31b7c9c142edc9c4fcc146e7cf4ac2444567819d
kuavinash <avinash.kumar329@gmail.com>
2018-04-30 14:20:20 +0530

    removed create server text

--- app.js
+++ app.js
@@ -1,6 +1,2 @@
 var http = require('http');
 console.log('Hello Avinash');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-}).listen(8080);
