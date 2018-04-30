1dd7458c588ac1b881a130873770d5d309d53df9
kuavinash <avinash.kumar329@gmail.com>
2018-04-30 13:56:54 +0530

    added node modules

---
+++ app.js
@@ -0,0 +1,6 @@
+var http = require('http');
+console.log('Hello Avinash');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}).listen(8080);
